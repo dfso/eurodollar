14f22438e0447e93e01be6bece22c5c066440e72
dfso <denison.new@gmail.com>
2020-11-12 00:58:40 -0400

    obtendo valores de USD e EUR

---
+++ pages/index.js
@@ -0,0 +1,27 @@
+import Head from "next/head";
+function Home({ usd_brl, eur_brl }) {
+  return (
+    <div style={{ textAlign: "center"}}>
+      <Head>
+        <title>EURODOLLAR</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+      </Head>
+      <h1><i class="fas fa-money-check-alt"></i></h1>
+      <h2><i class="fas fa-dollar-sign"></i> = {usd_brl} BRL</h2>
+      <h2><i class="fas fa-euro-sign"></i> = {eur_brl} BRL</h2>
+    </div>
+  );
+}
+
+Home.getInitialProps = async (ctx) => {
+  const usd = await fetch("https://api.exchangeratesapi.io/latest?base=USD");
+  const eur = await fetch("https://api.exchangeratesapi.io/latest?base=EUR");
+  const json_usd = await usd.json();
+  const json_eur = await eur.json();
+  return {
+    usd_brl: json_usd.rates["BRL"],
+    eur_brl: json_eur.rates["BRL"],
+  };
+};
+export default Home;
